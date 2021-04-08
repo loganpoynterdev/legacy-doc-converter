2479e034fc6051898a14cd23e46bfdfcb7f73c1a
Logan Poynter <42699943+loganpoynterdev@users.noreply.github.com>
2021-04-08 15:09:00 -0400

    Update legacy-doc-converter.ps1
    Further cleanup

--- legacy-doc-converter.ps1
+++ legacy-doc-converter.ps1
@@ -1,10 +1,10 @@
-﻿function Select-FileDialog
+function Select-FileDialog
 {
 	param([string]$Description,[string]$Directory,[string]$Filter="All Files (*.*)|*.*")
 	[System.Reflection.Assembly]::LoadWithPartialName("System.Windows.Forms") | Out-Null
 	$objForm = New-Object System.Windows.Forms.FolderBrowserDialog
-    	$objForm.RootFolder = "Desktop"
-    	$objForm.Description = $Description
+    $objForm.RootFolder = "Desktop"
+    $objForm.Description = $Description
 	$Show = $objForm.ShowDialog()
 	If ($Show -eq "OK")
 	{
@@ -16,44 +16,38 @@
 	}
 }
 
-$containingDir = Select-FileDialog -Description "Pick A Folder"
+$containingDir = Select-FileDialog -Description "Pick containing folder of legacy documents..."
 
 [ref]$SaveFormat = "microsoft.office.interop.word.WdSaveFormat" -as [type]
-$1word = new-object -ComObject Word.Application
-$1word.Visible = $False
+$wordInstance = new-object -ComObject Word.Application
+$wordInstance.Visible = $False
 
 $filesToConvert = Get-ChildItem $containingDir | where{$_.Extension -eq ".doc" } 
+
 if($filesToConvert){
-write-host Found $filesToConvert.Count word 97 docs in: $containingDir -ForegroundColor Green
-forEach($EV in $filesToConvert) {
-        write-host "Converting :" $EV.fullname -ForegroundColor Green 
-        [ref]$name = Join-Path -Path $EV.DirectoryName -ChildPath $($EV.BaseName + ".docx")
-        $opendoc = $1word.documents.open($EV.FullName)
-        $opendoc.saveas([ref]$name.Value, [ref]$saveFormat::wdFormatDocument)
-        $opendoc.saveas([ref]$name.Value, [ref]$SaveFormat::wdFormatDocument)
-        [ref]$saveFormat::wdFormatDocument
-        $opendoc.close()
-        $EV = $null
-    }
+    write-host Found $filesToConvert.Count legacy docs in: $containingDir -ForegroundColor Green
+    forEach($EV in $filesToConvert) {
+            write-host "Converting :" $EV.fullname -ForegroundColor Green 
+            [ref]$name = Join-Path -Path $EV.DirectoryName -ChildPath $($EV.BaseName + ".docx")
+            $opendoc = $wordInstance.documents.open($EV.FullName)
+            $opendoc.saveas([ref]$name.Value, [ref]$saveFormat::wdFormatDocument)
+            $opendoc.saveas([ref]$name.Value, [ref]$SaveFormat::wdFormatDocument)
+            [ref]$saveFormat::wdFormatDocument
+            $opendoc.close()
+            $EV = $null
+        }
 
-write-host Doing cleanup -ForegroundColor Green `n `n `n
-get-childitem $containingDir | where{$_.extension -eq ".doc" } | remove-item
-Set-location $containingDir
-attrib +U -P /S
+    $wordInstance.quit()
 
-$1word.quit()
-
-    $null = [System.Runtime.InteropServices.Marshal]::ReleaseComObject([System.__ComObject]$1word)
+    $null = [System.Runtime.InteropServices.Marshal]::ReleaseComObject([System.__ComObject]$wordInstance)
     [gc]::Collect()
     [gc]::WaitForPendingFinalizers()
-    Remove-Variable 1word
+    Remove-Variable wordInstance
 
-write-host All Done! -ForegroundColor Green 
-
-write-host Closing in 15 seconds! -ForegroundColor Yellow
-sleep -seconds 15
+write-host All Done! Closing in 15 seconds! -ForegroundColor Green 
+sleep -Seconds 15
 }
 else{
-    write-host No word 97 files detected in $containingDir
+    write-host No legacy files detected in $containingDir
     sleep -Seconds 15
 }
